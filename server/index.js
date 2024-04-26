c015a69e59ecc4d9ab1fccd0efc035ae38cb4922
Raton117 <ratonkhan7799@gmail.com>
2024-04-27 00:28:56 +0600

    schema and data added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 import express from 'express';
 import db from './database/db.js';
+import dotenv from 'dotenv';
+import DefaultData from './default.js';
+
 const app = express();
 
-db();
+
+
+dotenv.config();
+
+const username=process.env.DB_USERNAME;
+const password=process.env.DB_PASSWORD;
+
+db(username,password);
+
 app.listen(8000, () => {
   console.log('Server is running on port 8000');
 });
+
+DefaultData();
